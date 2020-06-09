62d0cc40136f885c482a96c85d0aead4b1d1f1ea
Ryan Lane <ryanml1999@gmail.com>
2020-06-09 16:52:45 -0700

    better comments for main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-// Test
-
 #include <iostream>
 #include "Hand.h"
 #include "Deck.h"
@@ -8,21 +6,25 @@ using namespace std;
 
 int main()
 {
+  // declare deck and hand objects
   Deck deck;
   Hand dealer_hand;
   Hand player_hand;
 
+  // TEST drawing cards and adding them to hands
   for(int i = 0; i < 3; i++)
     dealer_hand.add_card(deck.draw());
 
   for(int i = 0; i < 3; i++)
     player_hand.add_card(deck.draw());
 
+  // display hands
   cout << "Dealer hand: ";
   dealer_hand.print();
   cout << "Player hand: ";
   player_hand.print();
 
+  // display sums
   cout << endl;
   cout << "The sum of the dealer hand is: " << dealer_hand.sum();
   if(dealer_hand.sum() > BUST)
